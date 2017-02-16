82a8a078493eeee32d41c81bef7ee48df2921b61
Jim Walker <jim.walkster@gmail.com>
2017-02-16 04:45:35 -0700

    Add openvpn filename to output

--- pingopenvpnservers.sh
+++ pingopenvpnservers.sh
@@ -11,6 +11,7 @@ vpnfiles=()
 hosts=()
 bestping="9999"
 besthost="unknown"
+bestfile="unknown"
 
 #Read files from directory and get connection info
 hostdata=$(grep -nr "remote $vpnscope" $vpndir)
@@ -24,19 +25,27 @@ while IFS=: read -r filename dud host; do
 done <<< "$hostdata"
 
 #Loop over host array to find best ping
-for h in "${hosts[@]}"
-do
-   
-   p1=$(ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
-   p2=$(ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
-   p3=$(ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
-   p4=$(ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
-   p5=$(ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
+arrayindices=${!hosts[*]}
 
-   if [[ $p1 && $p2 && $p3 && $p4 && $p5 ]] 
+#for h in "${hosts[@]}"
+for index in $arrayindices;
+do
+   f=${vpnfiles[$index]}
+   h=${hosts[$index]}
+   #echo "$f $h $index"
+   
+   p1=$(timeout 1 ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
+   p2=$(timeout 1 ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
+   p3=$(timeout 1 ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
+   #p4=$(ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
+   #p5=$(ping -c 1 $h | grep -o time=[0-9].* | cut -c 6-12)
+
+   #if [[ $p1 && $p2 && $p3 && $p4 && $p5 ]] 
+   if [[ $p1 && $p2 && $p3 ]] 
    then
 	  # Sum up floats
-	  sum=$(echo "scale=2;$p1 + $p2 + $p3 + $p4 + $p5" | bc)
+	  #sum=$(echo "scale=2;$p1 + $p2 + $p3 + $p4 + $p5" | bc)
+	  sum=$(echo "scale=2;$p1 + $p2 + $p3" | bc)
 	  avg=$(echo "scale=2;$sum / 5" | bc)
 	  
 	  if [ "$1" != "-best" ]
@@ -48,7 +57,8 @@ do
 	  if [ $(echo "$avg < $bestping" | bc) -eq 1 ]
 	  then
 	     bestping=$avg
-	     besthost="$h"
+	     besthost=$h
+		 bestfile=$f
 	  fi
    else
       if [ "$1" != "-best" ]
@@ -62,5 +72,5 @@ done
 
 if [ "$1" == "-best" ]
 then 
-   echo "$besthost $bestping"
+   echo "$bestfile,$besthost,$bestping"
 fi
